d15efd2ba077074f7b2c4c84012f0e763a621611
tomato645 <89794096+tomato645@users.noreply.github.com>
2023-10-17 20:39:14 +0900

    clean

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-async function changeBackgroudColor(){
+async function changeBackgroudColor() {
     let randomNum = random(1000, 5000);
     console.log(`waiting ${randomNum} ms...`)
     await sleep(randomNum);
     console.log("change color");
     document.getElementById("click_space").style.backgroundColor = "red";
     let changedTime = performance.now();
-    
+
     let clickedTime = await clickWaiter();
-    
-    let time = clickedTime-changedTime;
+
+    let time = clickedTime - changedTime;
     localStorage.setItem(getDate(), time);
     document.getElementById("time").innerHTML = `${time} ms`;
     console.log(`time: ${time} ms`);
 }
 
-let clickWaiter = () => new Promise((resolve) => document.getElementById("click_space").onclick = function(){
+let clickWaiter = () => new Promise((resolve) => document.getElementById("click_space").onclick = function () {
     console.log("clicked");
     document.getElementById("click_space").style.backgroundColor = "white";
     let clickedTime = performance.now();
     resolve(clickedTime);
 })
 
-function sleep(ms){
+function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function random(min, max){
+function random(min, max) {
     return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-function getDate(){
+function getDate() {
     let date = new Date();
-    return date.getFullYear() + '' + ('0' + (date.getMonth() + 1)).slice(-2) + '' +('0' + date.getDate()).slice(-2) + '' +  ('0' + date.getHours()).slice(-2) + '' + ('0' + date.getMinutes()).slice(-2) + '' + ('0' + date.getSeconds()).slice(-2) + '' + date.getMilliseconds()
+    return date.getFullYear() + '' + ('0' + (date.getMonth() + 1)).slice(-2) + '' + ('0' + date.getDate()).slice(-2) + '' + ('0' + date.getHours()).slice(-2) + '' + ('0' + date.getMinutes()).slice(-2) + '' + ('0' + date.getSeconds()).slice(-2) + '' + date.getMilliseconds()
 }
 
-window.onload = function() {
+window.onload = function () {
     let startStopBtn = document.querySelector("#start-stop-btn");
-    startStopBtn.addEventListener("click", async function() {
+    startStopBtn.addEventListener("click", async function () {
         console.log("toggle");
         for (let i = 0; i < 10; i++) {
-            if (startStopBtn.checked === false){
+            if (startStopBtn.checked === false) {
                 console.log("break");
                 break
             }
